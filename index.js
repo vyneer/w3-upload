c0b777eddf071b46b82aec2928938b8cf84c0430
vyneer <vyn33r@gmail.com>
2022-09-19 13:29:34 +0300

    trying odysee out

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import process from 'process'
 import minimist from 'minimist'
-import { Web3Storage, getFilesFromPath } from 'web3.storage'
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 import * as fs from 'fs'
@@ -8,35 +7,19 @@ import * as nodepath from 'path'
 import moment from 'moment'
 import * as gvd from 'get-video-duration'
 
-async function storeWithProgress(client, files, name) {
-  // show the root cid as soon as it's ready
-  const onRootCidReady = cid => {
-    console.log('uploading files with cid:', cid)
-  }
-
-  // when each chunk is stored, update the percentage complete and display
-  const totalSize = files.map(f => f.size).reduce((a, b) => a + b, 0)
-  let uploaded = 0
-
-  const onStoredChunk = size => {
-    uploaded += size
-    const pct = (uploaded / totalSize) * 100
-    console.log(`Uploading... ${pct.toFixed(2)}% complete`)
-  }
-
-  // client.put will invoke our callbacks during the upload
-  // and return the root cid when the upload completes
-  return client.put(files, { onRootCidReady, onStoredChunk, name: name })
-}
-
 async function main () {
   const args = minimist(process.argv.slice(2))
-  const token = args.token
+  const lbryurl = args.lbryurl
+  const lbrychannel = args.lbrychannel
   const dbpath = args.db
   const path = args.path
-  
-  if (!token) {
-    return console.error('A token is needed. You can create one on https://web3.storage')
+
+  if (!lbrychannel) {
+    return console.error('Please supply the lbry/odysee channel name')
+  }
+
+  if (!lbryurl) {
+    return console.error('Please supply the lbrynet API url')
   }
 
   if (!dbpath) {
@@ -48,10 +31,6 @@ async function main () {
   }
 
   const platform = path.includes("YOUTUBE") ? "youtube" : "twitch";
-
-  const storage = new Web3Storage({ token })
-
-  const files = await getFilesFromPath(path)
 
   const folderName = nodepath.parse(path).base
 
@@ -74,46 +53,82 @@ async function main () {
   
   const startTime = moment(folderName.match(/\d{8}_\d{6}/gm)[0], "YYYYMMDD_HHmmss")
   console.log(startTime)
-  const duration = await gvd.getVideoDurationInSeconds(`${path}/index.m3u8`)
+  const duration = await gvd.getVideoDurationInSeconds(`${path}/${videoID}.mp4`)
   console.log(duration)
   const endTime = startTime.clone().add(duration, "s")
   console.log(endTime)
+  console.log(`${videoID} - ${videoInfo.title.replace(/[^a-zA-Z0-9\- ]/g, "_")}`);
 
-  const cid = await storeWithProgress(storage, files, `${videoID}_${folderName}`)
-  console.log('Content added with CID:', cid)
+  const uploadData = {
+    "method": "publish", 
+    "params": {
+      "name": `${videoID}-r-${Math.floor(Math.random() * (100) + 1)}`, 
+      "title": `[${videoID}] - ${videoInfo.title}`, 
+      "bid": "0.0001", 
+      "file_path": `/home/${path.slice(path.indexOf("captured/"))}/${videoID}.mp4`, 
+      "validate_file": false, 
+      "optimize_file": false, 
+      "author": "Destiny",
+      "tags": [
+        "destiny",
+        "streamer",
+        "vod",
+        startTime.format("YYYY-MM-DD HH:mm:ss"),
+        "politics"
+      ], 
+      "languages": [
+        "en"
+      ], 
+      "locations": [], 
+      "channel_name": lbrychannel, 
+      "wallet_id": "default_wallet",
+      "funding_account_ids": [], 
+      "preview": false, 
+      "blocking": false
+    }
+  }
 
-  open({
-    filename: dbpath,
-    driver: sqlite3.Database
-  }).then(async (db) => {
-    db.exec(
-      `create table if not exists ipfs (
-           timest integer,
-           platform text,
-           id text,
-           username text,
-           title text,
-           starttime text,
-           endtime text, 
-           thumbnail text,
-           cid text,
-           folder text
-       )`
-    )
-    db.run("INSERT INTO ipfs (platform, timest, id, username, title, starttime, endtime, thumbnail, cid, folder) VALUES ($platform, $timest, $id, $username, $title, $start, $end, $thumbnail, $cid, $folder)", {
-      $platform: platform.toLowerCase(),
-      $timest: Date.now(),
-      $id: videoID,
-      $username: videoInfo.author_name,
-      $title: videoInfo.title,
-      $start: startTime.toISOString().slice(0,-5) + "Z",
-      $end: endTime.toISOString().slice(0,-5) + "Z",
-      $thumbnail: videoInfo.thumbnail_url.includes("hqdefault.jpg") ? `${videoInfo.thumbnail_url.slice(0, -14)}/mqdefault_live.jpg` : videoInfo.thumbnail_url,
-      $cid: cid,
-      $folder: folderName
-    })
-    db.close()
+  await fetch(lbryurl, {
+    method: "POST",
+    body: JSON.stringify(uploadData)
   })
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log("LBRY success:", data)
+    open({
+      filename: dbpath,
+      driver: sqlite3.Database
+    }).then(async (db) => {
+      db.exec(
+        `create table if not exists odysee (
+             timest integer,
+             platform text,
+             id text,
+             username text,
+             title text,
+             starttime text,
+             endtime text, 
+             thumbnail text,
+             url text
+         )`
+      )
+      db.run("INSERT INTO odysee (platform, timest, id, username, title, starttime, endtime, thumbnail, url) VALUES ($platform, $timest, $id, $username, $title, $start, $end, $thumbnail, $url)", {
+        $platform: platform.toLowerCase(),
+        $timest: Date.now(),
+        $id: videoID,
+        $username: videoInfo.author_name,
+        $title: videoInfo.title,
+        $start: startTime.toISOString().slice(0,-5) + "Z",
+        $end: endTime.toISOString().slice(0,-5) + "Z",
+        $thumbnail: videoInfo.thumbnail_url.includes("hqdefault.jpg") ? `${videoInfo.thumbnail_url.slice(0, -14)}/mqdefault_live.jpg` : videoInfo.thumbnail_url,
+        $url: `${data.result.outputs[0].permanent_url.substring(7).split("#")[0]}/${data.result.outputs[0].claim_id}`
+      })
+      db.close()
+    })
+  })
+  .catch((error) => {
+    console.error('LBRY error:', error);
+  });
 }
 
 main()
